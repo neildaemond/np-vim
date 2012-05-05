1839e99efedf68c180be026c85d2655b057c8f05
Neil Pahl <neildaemond@gmail.com>
2012-05-05 17:05:49 +0800

    updated README, added update_external_configs.sh

--- pushGIT.sh
+++ pushGIT.sh
@@ -1,3 +1,11 @@
-#!bin/bash
-cp ~/.vimrc ./
-git push
+#!/bin/bash
+
+#this script will help me update any configurations that are outside the ~/.vim folder
+
+#copy dot external dot files into git project
+cp ~/.vimrc ~/.vim/dotvimrc
+cp ~/.ctags ~/.vim/dotctags
+
+git add *
+git commit -m $1
+git push origin master
---
+++ update_external_configs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/.vim/dotvimrc ~/.vimrc
+cp ~/.vim/dotctags ~/.ctags
